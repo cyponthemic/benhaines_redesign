4b5909473a5d573006334b131b9ad05cb5fe437e
Alex CHAVET <alex.chavet@gmail.com>
2016-06-22 20:29:06 +1000

    Blog part 2

--- library/categories.php
+++ library/categories.php
@@ -7,12 +7,30 @@
  */
 
 
+function change_post_object_label()
+{
+    global $wp_post_types;
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'Articles';
+    $labels->singular_name = 'Article';
+    $labels->add_new = 'Add Article';
+    $labels->add_new_item = 'Add Article';
+    $labels->edit_item = 'Edit Article';
+    $labels->new_item = 'Article';
+    $labels->all_items = 'All Articles';
+    $labels->view_item = 'View Articles';
+    $labels->search_items = 'Search Articles';
+    $labels->not_found = 'No Articles found';
+    $labels->not_found_in_trash = 'No Articles found in Trash';
+}
+add_action( 'init', 'change_post_object_label' );
+add_action( 'admin_menu', 'change_post_menu_label' );
 function displayCategory() {
     $post_type = get_post_type_object( get_post_type($post) );
     echo '<span class="news--category--title">Category:&nbsp;</span>';
     echo '<a href="'. get_post_type_archive_link($post_type->name).'">';
     echo '<span class="news--category--category '.$post_type->name.'">';
-    echo $post_type->label ;
+    echo $post_type->labels->name ;
     echo '</span>';
     echo '</a>';
 
@@ -32,12 +50,9 @@ function listCategoryButton() {
 
         }
         //Change post to article
-        if($value === 'post'){
-            $name= 'articles';
-        }
-        else{
+
             $name= $post_type->labels->name;
-        }
+
         //Add active class
         if($template_post_type->name === $post_type->name){
             $isactive= ' active';
@@ -49,3 +64,5 @@ function listCategoryButton() {
     }
 }
 add_action( 'list_category_name', 'listCategoryButton' );
+
+

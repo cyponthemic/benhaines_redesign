3318a0add9b62a25f4950b4ef8f2ed9e3b6db1de
Alex CHAVET <alex.chavet@gmail.com>
2016-07-07 09:01:40 +1000

    Creating off canvas menu left for sidebars

--- template-parts/mobile-off-canvas.php
+++ template-parts/mobile-off-canvas.php
@@ -11,5 +11,9 @@
 <nav class="off-canvas position-right" id="mobile-menu" data-off-canvas data-position="right" role="navigation">
   <?php foundationpress_mobile_nav(); ?>
 </nav>
+<nav class="off-canvas position-left off-canvas-sidebar" id="mobile-menu-2" data-off-canvas data-position="left" role="navigation">
 
+  <?php get_sidebar('offcanvas'); ?>
+
+</nav>
 <div class="off-canvas-content" data-off-canvas-content>

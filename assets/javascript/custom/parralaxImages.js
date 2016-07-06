fc83df4de669c7358772bfa03a93a1bf33bacf06
Alex CHAVET <alex.chavet@gmail.com>
2016-07-07 09:04:33 +1000

    Fixing off canvas issue with parralax

--- assets/javascript/custom/parralaxImages.js
+++ assets/javascript/custom/parralaxImages.js
@@ -14,3 +14,35 @@ $(document).ready(function(){
         speed: 0.9
     });
 });
+
+
+//Fix off-canvas/parralax issues
+
+$(document).ready(function(){
+    //Check wich offcanvas is open
+
+    $(this).on( 'opened.zf.offcanvas' ,function (event){
+        console.log(event.target.attributes[3].nodeValue );
+        if(event.target.attributes[3].nodeValue == 'left'){
+            $('.parallax-mirror').addClass('offcanvas-open').removeClass('offcanvas-closed').removeClass('left');
+            $( ".js-off-canvas-exit" ).clone().appendTo( ".parallax-mirror").addClass('js-off-canvas-exit-cloned right');
+            console.log('right', event.target.attributes[3].nodeValue);
+        }
+        else {
+            $('.parallax-mirror').addClass('offcanvas-open').removeClass('offcanvas-closed').addClass('left');
+            $( ".js-off-canvas-exit" ).clone().appendTo(".parallax-mirror").addClass('js-off-canvas-exit-cloned left');
+            console.log('left', event.target.attributes[3].nodeValue);
+        }
+
+        $('.js-off-canvas-exit-cloned').on('click', function(){
+            $(event.target).foundation('close');
+        });
+
+    });
+    $(this).on( 'closed.zf.offcanvas' ,function (){
+        $('.parallax-mirror').removeClass('offcanvas-open').removeClass('offcanvas-closed').removeClass('left');
+        console.log('test 2 ');
+        $('.parallax-mirror').find('.js-off-canvas-exit-cloned').remove();
+    });
+
+})

6f4fa18c087d97daa38ce761c34d7a93a16a3038
Alex CHAVET <alex.chavet@gmail.com>
2017-01-14 19:29:48 +1100

    Splitting checkout in colums

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -32,36 +32,38 @@ if ( ! $checkout->enable_signup && ! $checkout->enable_guest_checkout && ! is_us
 
 ?>
 
-<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+<form name="checkout" method="post" class="checkout woocommerce-checkout row" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+	<div class="columns large-6">
+		<?php if ( sizeof( $checkout->checkout_fields ) > 0 ) : ?>
 
-	<?php if ( sizeof( $checkout->checkout_fields ) > 0 ) : ?>
+			<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+			<div class="col2-set" id="customer_details">
+				<div class="col-1">
+					<?php do_action( 'woocommerce_checkout_billing' ); ?>
+				</div>
 
-		<div class="col2-set" id="customer_details">
-			<div class="col-1">
-				<?php do_action( 'woocommerce_checkout_billing' ); ?>
+				<div class="col-2">
+					<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+				</div>
 			</div>
 
-			<div class="col-2">
-				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-			</div>
-		</div>
+			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
-	<?php endif; ?>
-
-	<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
-
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+		<?php endif; ?>
 	</div>
 
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+	<div class="columns large-6">
+		<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
 
-</form>
+		<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+
+		<div id="order_review" class="woocommerce-checkout-review-order">
+			<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+		</div>
+
+		<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+	</div>
+</form> 
 
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
